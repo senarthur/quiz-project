91ff4c1c414297b9b22aa93d5e10a0b1f4bc42ce
Arthur Sena <sena.arthur698@gmail.com>
2026-01-21 12:15:42 -0300

    Logic and Styles implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Quiz />
+      </main>
     </>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+        clearTimeout(timer);
+    }
+  }, [onTimeout, timeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress value={remainingTime} max={timeout} id="question-time" />;
+}
